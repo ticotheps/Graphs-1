96563b71fd3c2853dd7cf51b7b1187611a666ebd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-20 15:08:00 -0500

    Adds notes to notes.py

---
+++ notes.py
@@ -0,0 +1,34 @@
+# - Graph Data Structures
+  # - What are they?
+    #  - collections of data that are represented by nodes and the connections
+    #    between them.
+
+  # - What are they comprised of?
+    # - "Nodes" (or "vertices")
+      # - represent objects in a data set
+      # - i.e. - cities, animals, web pages
+    # - "Edges"
+      # - connections between vertices; can be BIDRECTIONAL (point in both
+      #   directions to indicate a two-way relationship)
+    # - "Weights"
+      # - the "cost" to travel across an edge
+      # - only assigned to edges (not nodes)
+      
+  # - What are they useful for?
+    # - illustrating or representing relationships between entities
+      # - i.e. - social networks, physical networks (cell towers), cities, etc.
+      
+  # - What kinds of graphs exist?
+    # - "Directed Graphs"
+      # - can only move in ONE direction along edges
+    # - "Undirected Graphs"
+      # - allows movement in BOTH directions along edges
+    # - "Weighted Graphs" 
+      # - allows the edges (between vertices/nodes) to have weights on them.
+    # - "Unweighted Graphs" 
+      # - does not allow the edges (between vertices/nodes) to have weights on them.
+    # - "Cyclic Graphs"
+      # - the edges of this graph allow you revisit at least one vertex during traversal.
+    # - "Acyclic Graphs"
+      # - nodes/vertices that can be visited only ONCE during traversal
+      
